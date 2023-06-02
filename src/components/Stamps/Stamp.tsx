ad01b1d333d2946fa9985c5cefba89e0c23b1b04
m19e <nexus.me7290@gmail.com>
2023-06-02 16:29:57 +0900

    feat: open modal from stamp-item

--- src/components/Stamps/Stamp.tsx
+++ src/components/Stamps/Stamp.tsx
@@ -8,10 +8,13 @@ type Props = {
 
 export const Stamp = ({ stamp }: Props) => {
   return (
-    <div className="p-1 w-44 max-w-full h-44 max-h-full bg-[#ece5d8] hover:bg-white rounded shadow transition-all hover:scale-110">
+    <label
+      htmlFor="modal"
+      className="p-1 w-44 max-w-full h-44 max-h-full bg-[#ece5d8] hover:bg-white rounded shadow transition-all hover:scale-110"
+    >
       <div className="flex justify-center items-center p-1 w-full h-full border-2 border-[#e0d2b6]">
         <BlurImage {...stamp} />
       </div>
-    </div>
+    </label>
   )
 }
--- src/components/Stamps/index.tsx
+++ src/components/Stamps/index.tsx
@@ -1,5 +1,6 @@
 import type { StampProps } from "types/cms"
 
+import { Modal } from "./Modal"
 import { Stamp } from "./Stamp"
 
 type Props = {
@@ -10,18 +11,21 @@ export const Stamps = ({ stamps }: Props) => {
   const items = stamps.map((s) => <Stamp key={s.id} stamp={s} />)
 
   return (
-    <div className="flex flex-col items-center space-y-3 min-h-screen bg-kirara">
-      <header className="flex justify-center items-center mt-3 w-full h-16">
-        <h1 className="p-2 text-2xl font-black text-[#637340] rounded sm:text-4xl bg-[#EEF2D0]/75 font-latego">
-          #emotwicon_genshin
-        </h1>
-      </header>
-      <main className="flex flex-col flex-1 items-center p-4 bg-[#637340]/50 rounded-box">
-        <div className="grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-          {items}
-        </div>
-      </main>
-      <footer className="flex justify-center items-center w-full h-16 bg-[#637340] sm:h-24"></footer>
-    </div>
+    <>
+      <div className="flex flex-col items-center space-y-3 min-h-screen bg-kirara">
+        <header className="flex justify-center items-center mt-3 w-full h-16">
+          <h1 className="p-2 text-2xl font-black text-[#637340] rounded sm:text-4xl bg-[#EEF2D0]/75 font-latego">
+            #emotwicon_genshin
+          </h1>
+        </header>
+        <main className="flex flex-col flex-1 items-center p-4 bg-[#637340]/50 rounded-box">
+          <div className="grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+            {items}
+          </div>
+        </main>
+        <footer className="flex justify-center items-center w-full h-16 bg-[#637340] sm:h-24"></footer>
+      </div>
+      <Modal />
+    </>
   )
 }
