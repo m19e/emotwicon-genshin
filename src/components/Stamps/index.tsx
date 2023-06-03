933a404060d08334284b5392a504c51bffbb7f77
m19e <nexus.me7290@gmail.com>
2023-06-04 04:04:04 +0900

    feat: design toggle favorite button in header

--- src/components/Stamps/index.tsx
+++ src/components/Stamps/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import type { StampProps } from "types/cms"
 
 import { Modal } from "./Modal"
@@ -12,12 +13,8 @@ export const Stamps = ({ stamps }: Props) => {
 
   return (
     <>
-      <div className="flex flex-col items-center space-y-3 min-h-screen bg-kirara">
-        <header className="flex justify-center items-center mt-3 w-full h-16">
-          <h1 className="p-2 text-2xl font-black text-[#637340] rounded sm:text-4xl bg-[#EEF2D0]/75 font-latego">
-            #emotwicon_genshin
-          </h1>
-        </header>
+      <div className="flex flex-col items-center space-y-1.5 min-h-screen sm:space-y-3 bg-kirara">
+        <Header />
         <main className="flex flex-col flex-1 items-center p-4 min-w-full rounded-none lg:min-w-min lg:rounded-md bg-[#637340]/25">
           <div className="grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
             {items}
@@ -27,5 +24,64 @@ export const Stamps = ({ stamps }: Props) => {
       </div>
       <Modal />
     </>
+  )
+}
+
+const Header = () => {
+  const [favChecked, setFavChecked] = useState(false)
+
+  return (
+    <header className="px-4 mt-1.5 w-full sm:px-8 sm:mt-3 sm:h-16 lg:max-w-screen-lg navbar">
+      <div className="sm:navbar-start"></div>
+      <h1 className="p-2 text-2xl font-black text-[#637340] rounded sm:text-4xl bg-[#EEF2D0]/75 font-latego navbar-center">
+        #emotwicon_genshin
+      </h1>
+      <div className="navbar-end">
+        <div
+          className="flex gap-1 items-center p-2 bg-[#EEF2D0] shadow rounded-box"
+          onClick={() => setFavChecked((prev) => !prev)}
+        >
+          <input
+            type="checkbox"
+            className="checked:bg-[#d9b1ad] checked:border-[#d9b1ad] toggle"
+            checked={favChecked}
+          />
+          <label
+            className={"swap swap-flip " + (favChecked ? "swap-active" : "")}
+          >
+            <div className="swap-on">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                className="w-6 h-6 fill-[#d9b1ad] stroke-[#d9b1ad]"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                />
+              </svg>
+            </div>
+            <div className="swap-off">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                className="w-6 h-6 stroke-[#d9b1ad]"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                />
+              </svg>
+            </div>
+          </label>
+        </div>
+      </div>
+    </header>
   )
 }
