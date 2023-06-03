d83508115e39f09d4793a57cf22cf382803e63c4
m19e <nexus.me7290@gmail.com>
2023-06-03 14:19:41 +0900

    feat: select stamp from stamp-item

--- src/components/Stamps/Stamp.tsx
+++ src/components/Stamps/Stamp.tsx
@@ -1,3 +1,4 @@
+import { useSelectedStamp } from "hooks"
 import type { StampProps } from "types/cms"
 
 import { BlurImage } from "./BlurImage"
@@ -7,13 +8,16 @@ type Props = {
 }
 
 export const Stamp = ({ stamp }: Props) => {
+  const [, selectStamp] = useSelectedStamp()
+
   return (
     <label
       htmlFor="modal"
       className="p-1 w-44 max-w-full h-44 max-h-full bg-[#ece5d8] hover:bg-white rounded shadow transition-all hover:scale-110"
+      onClick={() => selectStamp(stamp)}
     >
       <div className="flex justify-center items-center p-1 w-full h-full border-2 border-[#e0d2b6]">
-        <BlurImage {...stamp} />
+        <BlurImage imageProps={stamp.imageProps} alt={stamp.alt} />
       </div>
     </label>
   )
