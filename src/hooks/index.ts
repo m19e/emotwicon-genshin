b174815b420df3f42033a4aaf51c16ac612ebb6d
m19e <nexus.me7290@gmail.com>
2023-06-04 14:08:15 +0900

    feat: add favorites hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,27 @@
 import { useAtom } from "jotai"
 
-import { selectedStampAtom } from "stores"
+import { selectedStampAtom, favoritesAtom, favoriteModeAtom } from "stores"
 
 export const useSelectedStamp = () => useAtom(selectedStampAtom)
+
+export const useFavorites = () => {
+  const [favoriteMode, setMode] = useAtom(favoriteModeAtom)
+  const [favorites, setFavs] = useAtom(favoritesAtom)
+
+  const toggleMode = () => setMode((prev) => !prev)
+  const updateFavs = (id: string) => {
+    const isDelete = favorites.includes(id)
+    if (isDelete) {
+      setFavs((prev) => {
+        const newFavs = prev.filter((f) => f !== id)
+        if (!newFavs.length) setMode(false)
+        return newFavs
+      })
+    } else {
+      setFavs((prev) => [...prev, id])
+    }
+  }
+  const disabled = !favorites.length
+
+  return { favoriteMode, favorites, disabled, toggleMode, updateFavs }
+}
