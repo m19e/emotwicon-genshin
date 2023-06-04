61de267796b16cec71f5368ab058348fffbb235d
m19e <nexus.me7290@gmail.com>
2023-06-04 14:06:55 +0900

    feat: add atoms about favorites

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,5 +1,9 @@
 import { atom } from "jotai"
+import { atomWithStorage } from "jotai/utils"
 import { mockKirara } from "consts"
 import type { StampProps } from "types/cms"
 
 export const selectedStampAtom = atom<StampProps>(mockKirara)
+
+export const favoriteModeAtom = atom(false)
+export const favoritesAtom = atomWithStorage<string[]>("favorites", [])
